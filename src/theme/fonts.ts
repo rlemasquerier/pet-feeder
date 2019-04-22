4e7dcfa5d9749afad76d9b589f76a14cd4b9111c
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-22 17:07:07 +0200

    Add fonts in theme

---
+++ src/theme/fonts.ts
@@ -0,0 +1,11 @@
+const fonts = {
+  regular: {
+    fontSize: 14,
+  },
+  strong: {
+    fontSize: 14,
+    fontWeight: 'bold' as 'bold',
+  },
+};
+
+export default fonts;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,13 @@
 import colors from './colors';
 import margins from './margins';
 import borders from './borders';
+import fonts from './fonts';
 
 const theme = {
   colors,
   margins,
   borders,
+  fonts,
 };
 
 export default theme;

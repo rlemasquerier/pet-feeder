581cd0a47a6171e138ba827cf1dba20b06e27a8e
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-23 00:05:22 +0200

    Add style to calendar

--- src/components/Calendar/Calendar.component.tsx
+++ src/components/Calendar/Calendar.component.tsx
@@ -1,11 +1,34 @@
 import React, { Component, ReactNode } from 'react';
+import { View, ViewStyle, StyleSheet } from 'react-native';
 import CalendarStrip from 'react-native-calendar-strip';
+import theme from '../../theme';
 import { locale } from './utils/locale';
 
 export class Calendar extends Component<{}, {}> {
   public render(): ReactNode {
     return (
-      <CalendarStrip style={{ height: 100, paddingTop: 20, paddingBottom: 10 }} locale={locale} />
+      <View style={styles.container}>
+        <CalendarStrip style={styles.calendar} locale={locale} />
+      </View>
     );
   }
 }
+
+interface Style {
+  container: ViewStyle;
+  calendar: ViewStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    backgroundColor: theme.colors.white,
+    ...theme.borders.shadow,
+  },
+  calendar: {
+    overflow: 'hidden',
+    backgroundColor: theme.colors.white,
+    height: 100,
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+});

840b3adca6f6095aae9184d7fe0020f7d732201f
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-22 17:26:04 +0200

    Add a top banner and a status bar on the home page

--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -1,28 +1,69 @@
 import React, { Component, ReactNode } from 'react';
-import { Text, View, ViewStyle, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  ViewStyle,
+  StyleSheet,
+  TextStyle,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 import { Card } from '../../components';
+import theme from '../../theme';
 
 export class Home extends Component {
   public render(): ReactNode {
     return (
-      <View style={styles.container}>
-        <Text>Welcome on Home</Text>
-        <Card title="Matin" content="Gaïa a été nourrie par Yoann !" />
-        <Card title="Soir" content="La gamelle de Gaïa est vide !" />
-      </View>
+      <>
+        <StatusBar barStyle="light-content" />
+        <SafeAreaView style={styles.statusBar} />
+        <SafeAreaView style={styles.container}>
+          <View style={styles.topBanner}>
+            <Text style={styles.topBannerText}>Bonjour Rodolphe !</Text>
+          </View>
+          <View style={styles.content}>
+            <Card title="Matin" content="Gaïa a été nourrie par Yoann !" />
+            <Card title="Soir" content="La gamelle de Gaïa est vide !" />
+          </View>
+        </SafeAreaView>
+      </>
     );
   }
 }
 
 interface Style {
+  statusBar: ViewStyle;
   container: ViewStyle;
+  content: ViewStyle;
+  topBanner: ViewStyle;
+  topBannerText: TextStyle;
 }
 
 const styles = StyleSheet.create<Style>({
+  statusBar: {
+    flex: 0,
+    backgroundColor: theme.colors.banner,
+  },
   container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  content: {
     flex: 1,
     paddingHorizontal: 20,
     alignItems: 'stretch',
     justifyContent: 'center',
+  },
+  topBanner: {
+    height: 50,
+    backgroundColor: theme.colors.banner,
+    paddingHorizontal: 2 * theme.margins.unit,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  topBannerText: {
+    ...theme.fonts.regular,
+    color: theme.colors.white,
   },
 });
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,12 +2,14 @@ const baseColors = {
   white: '#FFFFFF',
   black: '#000000',
   orange: '#F3B900',
+  blue: '#1C203F',
 };
 
 const styleguideColors = {
   white: baseColors.white,
   black: baseColors.black,
   primary: baseColors.orange,
+  banner: baseColors.blue,
 };
 
 export default {

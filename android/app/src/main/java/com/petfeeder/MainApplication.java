0de497e57189195c6738c962816fd140d4709b12
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-23 21:49:51 +0200

    Install react-native-vector icons and link

--- android/app/src/main/java/com/petfeeder/MainApplication.java
+++ android/app/src/main/java/com/petfeeder/MainApplication.java
@@ -3,6 +3,7 @@ package com.petfeeder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }

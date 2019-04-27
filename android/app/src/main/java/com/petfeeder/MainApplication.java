d9bf71e4a206bd30f37bf9925acea3a4e526dd1e
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-27 13:40:55 +0200

    Install react-navigation

--- android/app/src/main/java/com/petfeeder/MainApplication.java
+++ android/app/src/main/java/com/petfeeder/MainApplication.java
@@ -3,6 +3,7 @@ package com.petfeeder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );

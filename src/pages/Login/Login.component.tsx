9095a81b4a7e4490fc83aff2645a3411ff5ae66a
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-27 14:43:05 +0200

    Use LoginForm in Login page

--- src/pages/Login/Login.component.tsx
+++ src/pages/Login/Login.component.tsx
@@ -1,21 +1,26 @@
 import React, { Component, ReactNode } from 'react';
 import { Text, TextStyle, StyleSheet, View, ViewStyle, TouchableOpacity } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
+import { LoginForm } from './LoginForm';
+import { Page } from '../../components';
 import theme from './../../theme';
 
 export class Login extends Component<NavigationScreenProps, {}> {
   public render(): ReactNode {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Login Page</Text>
-        <TouchableOpacity
-          onPress={() => {
-            this.props.navigation.navigate('Home');
-          }}
-        >
-          <Text>Login</Text>
-        </TouchableOpacity>
-      </View>
+      <Page>
+        <View style={styles.container}>
+          <Text style={styles.text}>Login Page</Text>
+          <LoginForm />
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate('Home');
+            }}
+          >
+            <Text>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </Page>
     );
   }
 }
@@ -28,7 +33,7 @@ interface Style {
 const styles = StyleSheet.create<Style>({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
   text: {

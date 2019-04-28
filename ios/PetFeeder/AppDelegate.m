49fe75888342f107c3226840e9829e2b907143e9
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-04-28 21:57:50 +0200

    Install firebase for iOS

--- ios/PetFeeder/AppDelegate.m
+++ ios/PetFeeder/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -28,6 +30,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 

a454cc83486c2d0cb8aa1e07dc4ffaa6825c8940
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-07-28 17:03:05 +0200

    feat: Add profile picture placholder

--- src/theme/imagesMapper.ts
+++ src/theme/imagesMapper.ts
@@ -1,6 +1,7 @@
 const images = {
   loginBackground: require('./images/login-background.png'),
   cheersGif: require('./images/cheers.gif'),
+  profilePicturePlaceholder: require('./images/profile-picture-placeholder.jpg'),
 };
 
 export default images;

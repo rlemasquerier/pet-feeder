2a4a5a6d42409d73f81543a505adf2c3dd7997bc
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-06 21:31:15 +0200

    feat: Display animated header on statistics page

--- src/pages/Statistics/Statistics.component.tsx
+++ src/pages/Statistics/Statistics.component.tsx
@@ -1,13 +1,78 @@
 import React, { Component, ReactNode } from 'react';
-import { Page, TopBanner, BarChart } from '../../components';
+import { Text, TextStyle, StyleSheet, ViewStyle } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { onScroll } from 'react-native-redash';
+import { RoundHeader, Page, BarChart } from '../../components';
+import theme from './../../theme';
 
-export class Statistics extends Component<{}, {}> {
+const HEADER = {
+  EXTENDED_HEIGHT: 250,
+  FOLDED_HEIGHT: 150,
+  EXTENDED_ROUNDNESS_RATIO: 0.9,
+};
+
+interface State {
+  scrollY: Animated.Value<number>;
+}
+
+export class Statistics extends Component<{}, State> {
+  public state = {
+    scrollY: new Animated.Value(0),
+  };
   public render(): ReactNode {
+    const translateY = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER.FOLDED_HEIGHT],
+      outputRange: [0, -HEADER.FOLDED_HEIGHT],
+      extrapolate: Animated.Extrapolate.CLAMP,
+    });
     return (
       <Page>
-        <TopBanner label="Statistiques" />
-        <BarChart />
+        <Animated.ScrollView
+          scrollEventThrottle={16}
+          onScroll={onScroll({ y: this.state.scrollY })}
+          contentContainerStyle={{ alignItems: 'center', paddingTop: HEADER.EXTENDED_HEIGHT }}
+        >
+          <BarChart />
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+          <Text style={styles.text}>Other stuffs</Text>
+        </Animated.ScrollView>
+        <RoundHeader
+          color={theme.colors.banner}
+          height={HEADER.EXTENDED_HEIGHT}
+          ratio={HEADER.EXTENDED_ROUNDNESS_RATIO}
+          translateY={translateY}
+        />
       </Page>
     );
   }
 }
+
+interface Style {
+  container: ViewStyle;
+  text: TextStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  text: {
+    ...theme.fonts.regular,
+    fontSize: 20,
+    paddingVertical: 10,
+  },
+});

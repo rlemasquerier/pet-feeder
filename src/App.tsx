8eea3c33c42e38d15723da0320325649ab1e1967
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-10 20:36:42 +0200

    feat: Persist authentication data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppNavigator } from './AppNavigator';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { setTopLevelNavigator } from './services/navigation';
 
 export class App extends React.Component {
   public render(): JSX.Element {
     return (
       <Provider store={store}>
-        <AppNavigator ref={setTopLevelNavigator} />
+        <PersistGate loading={null} persistor={persistor}>
+          <AppNavigator ref={setTopLevelNavigator} />
+        </PersistGate>
       </Provider>
     );
   }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,7 @@
 import { applyMiddleware, compose, createStore as reduxCreateStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to AsyncStorage
 
 import { rootReducer } from './reducer';
 import { rootSaga } from './saga';
@@ -9,5 +11,14 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const enhancers = [applyMiddleware(sagaMiddleware)];
 
-export const store = reduxCreateStore(rootReducer, composeEnhancers(...enhancers));
+const persistConfig: PersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['authentication'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = reduxCreateStore(persistedReducer, composeEnhancers(...enhancers));
+export const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);

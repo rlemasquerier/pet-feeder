7c139d22982b7108cc97694e3fc18d13e7dd2573
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-15 18:41:59 +0200

    feat: Connect signup form

--- src/pages/Signup/Signup.component.tsx
+++ src/pages/Signup/Signup.component.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Formik } from 'formik';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { Formik, FormikActions } from 'formik';
 import { Page } from 'pet-feeder/src/components';
 import { SignupForm, Values } from './SignupForm';
+import { User, UserInput } from 'pet-feeder/src/types/types';
+import { MutationFunctionOptions, ExecutionResult } from 'react-apollo';
 
 const initialValues: Values = {
   email: '',
@@ -9,9 +13,56 @@ const initialValues: Values = {
   name: '',
 };
 
+interface UserMutationData {
+  createUser: User;
+}
+
+type AddUserMutationType = (
+  options?: MutationFunctionOptions<UserMutationData, UserInput>
+) => Promise<void | ExecutionResult<UserMutationData>>;
+
+const ADD_USER = gql`
+  mutation createUser($email: String!, $password: String!, $name: String!, $role: String!) {
+    createUser(input: { email: $email, password: $password, name: $name, role: $role }) {
+      id
+      name
+      email
+      role
+    }
+  }
+`;
+
+const handleSignup = async ({
+  values,
+  addUserMutation,
+  resetForm,
+}: {
+  values: Values;
+  addUserMutation: AddUserMutationType;
+  resetForm: (nextValues?: Values) => void;
+}) => {
+  const addUserResult = await addUserMutation({
+    variables: {
+      email: values.email,
+      password: values.password,
+      name: values.name,
+      role: 'feeder',
+    },
+  });
+  if (addUserResult && addUserResult.data && addUserResult.data.createUser) {
+    resetForm();
+  }
+};
+
 export const Signup: React.FC<null> = () => {
-  const onSubmitForm = (values: Values) => {
-    console.log(values);
+  const [addUserMutation] = useMutation<UserMutationData, UserInput>(ADD_USER);
+
+  const onSubmitForm = async (values: Values, { resetForm }: FormikActions<Values>) => {
+    handleSignup({
+      values,
+      addUserMutation,
+      resetForm,
+    });
   };
   return (
     <Page>

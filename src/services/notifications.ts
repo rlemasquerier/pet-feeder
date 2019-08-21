d3cc5e58593537511ea68ddbdc9140b0464d0847
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-21 22:23:30 +0200

    feat: Handle push notifs permissions

---
+++ src/services/notifications.ts
@@ -0,0 +1,32 @@
+import { AsyncStorage } from 'react-native';
+import firebase from 'react-native-firebase';
+
+const getToken = async () => {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  if (!fcmToken) {
+    fcmToken = await firebase.messaging().getToken();
+    if (fcmToken) {
+      // user has a device token
+      await AsyncStorage.setItem('fcmToken', fcmToken);
+    }
+  }
+};
+
+const requestPermission = async () => {
+  try {
+    await firebase.messaging().requestPermission();
+    // User has authorised
+    getToken();
+  } catch (error) {
+    // User has rejected permissions
+  }
+};
+
+export const checkPermission = async () => {
+  const enabled = await firebase.messaging().hasPermission();
+  if (enabled) {
+    getToken();
+  } else {
+    requestPermission();
+  }
+};

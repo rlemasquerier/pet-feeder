3051483f3050a5a4a1ef6299fc27e15ede600128
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-31 22:07:02 +0200

    fix: Use async storage from react native community

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { applyMiddleware, compose, createStore as reduxCreateStore, Action } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore, persistReducer, PersistConfig, PersistPartial } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to AsyncStorage
+import AsyncStorage from '@react-native-community/async-storage';
 
 import { rootReducer, RootState } from './reducer';
 import { rootSaga } from './saga';
@@ -13,7 +13,7 @@ const enhancers = [applyMiddleware(sagaMiddleware)];
 
 const persistConfig: PersistConfig = {
   key: 'root',
-  storage,
+  storage: AsyncStorage,
   whitelist: ['authentication'],
 };
 

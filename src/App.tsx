c88b99a5b1d76fe04c1fee4ce6292bb9f463dd34
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-31 22:15:58 +0200

    fix: Ignore lottie related warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -6,6 +7,10 @@ import { AppNavigator } from './AppNavigator';
 import { persistor, store } from './redux/store';
 import { setTopLevelNavigator } from './services/navigation';
 import { client } from './apollo/initClient';
+
+// Remove this and YellowBox after this issue is resolved https://github.com/expo/expo/issues/4455
+YellowBox.ignoreWarnings(['LottieAnimationView.getConstants']);
+YellowBox.ignoreWarnings(["UIManager['LottieAnimationView']"]);
 
 export class App extends React.Component {
   public render(): JSX.Element {

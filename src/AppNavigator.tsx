9ede78c0e29252ba74cd9dd912ba1332b759a0e6
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-08-31 21:54:33 +0200

    chore: Upgrade react navigation to 4.0.0

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -6,9 +6,10 @@ import {
   createAppContainer,
   NavigationContainer,
   createSwitchNavigator,
-  createBottomTabNavigator,
-  createStackNavigator,
+  NavigationDescriptor,
 } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Icon } from './components';
 import { IconName } from './components/Icon/Icon.component';
 import theme from './theme';
@@ -45,9 +46,9 @@ const ConnectedTabNavigator = createBottomTabNavigator(
     },
   },
   {
-    defaultNavigationOptions: ({ navigation }) => ({
+    defaultNavigationOptions: ({ navigation }: { navigation: NavigationDescriptor }) => ({
       showLabel: false,
-      tabBarIcon: ({ focused }) => {
+      tabBarIcon: ({ focused }: { focused: boolean }) => {
         const { routeName } = navigation.state;
         const color = focused ? theme.colors.primary : theme.colors.secondaryAction;
         let iconName: IconName;
@@ -80,7 +81,7 @@ const ConnectedTabNavigator = createBottomTabNavigator(
   }
 );
 
-const OnboardingStack: NavigationContainer = createStackNavigator(
+const OnboardingStack = createStackNavigator(
   {
     Login: {
       screen: Pages.Login,

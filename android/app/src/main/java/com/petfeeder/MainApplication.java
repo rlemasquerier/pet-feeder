e5bc5c43d41c593856ccdc9fc9106c0e367361a2
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-10-21 22:18:38 +0200

    chore(android): Upgrade react native to 0.61.2

--- android/app/src/main/java/com/petfeeder/MainApplication.java
+++ android/app/src/main/java/com/petfeeder/MainApplication.java
@@ -1,8 +1,12 @@
 package com.petfeeder;
 
 import android.app.Application;
-
+import android.content.Context;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -12,14 +16,6 @@ import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.auth.RNFirebaseAuthPackage;
-import io.invertase.firebase.database.RNFirebaseDatabasePackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                    
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
-import com.microsoft.codepush.react.CodePush;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
@@ -29,11 +25,6 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-        @Override
-        protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
-        }
     
     @Override
     public boolean getUseDeveloperSupport() {
@@ -44,27 +35,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage(),
-            new AsyncStoragePackage(),
-            new LottiePackage(),
-            new ImagePickerPackage(),
-            new VectorIconsPackage(),
-            new ReactNativeFirebaseAppPackage(),
-            new ReactNativeFirebaseMessagingPackage(),
-            new SvgPackage(),
-          new ImagePickerPackage(),
-          new AsyncStoragePackage(),
-          new ReanimatedPackage(),
-          new SvgPackage(),
-          new RNFirebasePackage(),
-          new RNFirebaseAuthPackage(),
-          new RNFirebaseDatabasePackage(),
           new RNGestureHandlerPackage(),
-          new RNFirebaseMessagingPackage(),
-          new RNFirebaseNotificationsPackage(),
+          new AsyncStoragePackage(),
+          new LottiePackage(),
+          new ImagePickerPackage(),
           new VectorIconsPackage(),
-          new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
-          new LottiePackage()
+          new ReactNativeFirebaseAppPackage(),
+          new ReactNativeFirebaseMessagingPackage(),
+          new SvgPackage(),
+          new ReanimatedPackage()
       );
     }
 

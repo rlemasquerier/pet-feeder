fd22be780f5bc6a6d57c9de0f9bcce2a3b487759
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-11-13 00:23:50 +0400

    refactor: Create tribe in a separate component

--- src/pages/JoinOrCreateTribe/JoinOrCreateTribe.tsx
+++ src/pages/JoinOrCreateTribe/JoinOrCreateTribe.tsx
@@ -1,86 +1,25 @@
 import React, { useState } from 'react';
 import { Text, TextStyle, StyleSheet, View, ViewStyle } from 'react-native';
 import { Formik } from 'formik';
-import { MutationFunctionOptions, ExecutionResult } from 'react-apollo';
-import { useMutation } from '@apollo/react-hooks';
 import { useCurrentUser } from 'pet-feeder/src/hooks';
-import { createTribe as createTribeMutation } from 'pet-feeder/src/graphql/mutations/createTribe';
 import theme from './../../theme';
 import { Page, TopBanner, TribeCode, Button, Loader } from 'pet-feeder/src/components';
-import { CreateTribeForm, Values as CreateTribeFormValues } from './CreateTribeForm';
 import { JoinTribeForm, Values as JoinTribeFormValues } from './JoinTribeForm';
-import { User, Tribe } from 'pet-feeder/src/types';
-import { createTribeCode } from 'pet-feeder/src/api/apiClient';
 import { navigator } from 'pet-feeder/src/services/navigation';
+import { CreateTribe } from './CreateTribe';
 
 interface Props {}
-
-interface CreateTribeMutationData {
-  createTribe: Tribe;
-}
-
-interface CreateTribeInput {
-  name: string;
-  members: string[];
-}
-
-type CreateTribeMutationType = (
-  options?: MutationFunctionOptions<CreateTribeMutationData, CreateTribeInput>
-) => Promise<void | ExecutionResult<CreateTribeMutationData>>;
-
-const initialCreateTribeValues: CreateTribeFormValues = {
-  name: '',
-};
 
 const initialJoinTribeValues: JoinTribeFormValues = {
   code: '',
 };
 
-const handleCreateTribe = async ({
-  values,
-  user,
-  createTribeMutation,
-}: {
-  values: CreateTribeFormValues;
-  user: User;
-  createTribeMutation: CreateTribeMutationType;
-}): Promise<Tribe | undefined> => {
-  try {
-    const createTribeAnswer = await createTribeMutation({
-      variables: {
-        name: values.name,
-        members: [user.id],
-      },
-    });
-    if (createTribeAnswer && createTribeAnswer.data) {
-      return createTribeAnswer.data.createTribe;
-    } else {
-      throw new Error('Create tribe answer not correct');
-    }
-  } catch (error) {
-    console.warn('An error occurred when creating a tribe');
-  }
-};
-
 export const JoinOrCreateTribe: React.FC<Props> = () => {
   const [code, setCode] = useState<string | null>(null);
-  const [createTribe] = useMutation<CreateTribeMutationData, CreateTribeInput>(createTribeMutation);
   const { user } = useCurrentUser();
   if (!user) {
     return <Loader size={100} />;
   }
-  const onSubmitCreateTribeForm = async (values: CreateTribeFormValues): Promise<void> => {
-    const createdTribe = await handleCreateTribe({
-      values,
-      user,
-      createTribeMutation: createTribe,
-    });
-    if (createdTribe) {
-      const createCodeResponse = await createTribeCode(createdTribe.id);
-      const code = createCodeResponse.data.code;
-      setCode(code);
-    }
-  };
   const onSubmitJoinTribeForm = async (values: JoinTribeFormValues): Promise<void> => {
     console.log('values', values);
   };
@@ -91,13 +30,7 @@ export const JoinOrCreateTribe: React.FC<Props> = () => {
         <Text style={styles.titleText}>Que souhaites-tu faire ?</Text>
         <View style={{ justifyContent: 'center' }}>
           <Text style={styles.subtitleText}>Créer une nouvelle coloc</Text>
-          {!code && (
-            <Formik
-              onSubmit={onSubmitCreateTribeForm}
-              initialValues={initialCreateTribeValues}
-              component={CreateTribeForm}
-            />
-          )}
+          {!code && <CreateTribe onCodeReceived={setCode} />}
           {!!code && (
             <>
               <Text style={{ marginVertical: 3 * theme.margins.unit }}>

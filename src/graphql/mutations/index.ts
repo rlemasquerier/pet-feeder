86fb3ff8507e3e33c9e2f86b5bd508f304d1305a
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-11-13 16:54:47 +0400

    feat: Handle tribe joining if code is valid

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -1,2 +1,4 @@
 export { updateUserNotificationsToken } from './updateUserNotificationsToken';
 export { updateUserProfilePictureUrl } from './updateUserProfilePicture';
+export { createTribe } from './createTribe';
+export { joinTribe } from './joinTribe';
--- src/pages/JoinOrCreateTribe/JoinTribe/index.tsx
+++ src/pages/JoinOrCreateTribe/JoinTribe/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Text, TextStyle, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
+import { MutationFunctionOptions, ExecutionResult } from 'react-apollo';
+import { useMutation } from '@apollo/react-hooks';
 import theme from 'pet-feeder/src/theme';
 import { JoinTribeForm, Values as JoinTribeFormValues } from './JoinTribeForm';
 import { checkTribeCode } from 'pet-feeder/src/api/apiClient';
+import { joinTribe as joinTribeMutation } from 'pet-feeder/src/graphql/mutations';
+import { Tribe } from 'pet-feeder/src/types';
+import { useCurrentUser } from 'pet-feeder/src/hooks';
+import { navigator, PAGES, PageNameType } from 'pet-feeder/src/services/navigation';
 
 interface Props {}
 
@@ -11,11 +17,62 @@ const initialJoinTribeValues: JoinTribeFormValues = {
   code: '',
 };
 
+interface JoinTribeMutationData {
+  addUserToTribe: Tribe;
+}
+
+interface JoinTribeInput {
+  tribeId: string;
+  userId: string;
+}
+
+type JoinTribeMutationType = (
+  options?: MutationFunctionOptions<JoinTribeMutationData, JoinTribeInput>
+) => Promise<void | ExecutionResult<JoinTribeMutationData>>;
+
+const handleJoinTribe = async ({
+  tribeId,
+  userId,
+  joinTribeMutation,
+}: {
+  tribeId: string;
+  userId: string;
+  joinTribeMutation: JoinTribeMutationType;
+}): Promise<Tribe | undefined> => {
+  try {
+    const joinTribeAnswer = await joinTribeMutation({
+      variables: {
+        tribeId,
+        userId,
+      },
+    });
+    if (joinTribeAnswer && joinTribeAnswer.data) {
+      return joinTribeAnswer.data.addUserToTribe;
+    } else {
+      throw new Error('Join tribe answer not correct');
+    }
+  } catch (error) {
+    console.warn('An error occurred when adding user to a tribe');
+    throw error;
+  }
+};
+
 export const JoinTribe: React.FC<Props> = () => {
+  const { user } = useCurrentUser();
+  if (!user) {
+    return null;
+  }
+  const [joinTribe] = useMutation<JoinTribeMutationData, JoinTribeInput>(joinTribeMutation);
   const onSubmitJoinTribeForm = async (values: JoinTribeFormValues): Promise<void> => {
     try {
       const res = await checkTribeCode(values.code);
       const tribeIdToJoin = res.data;
+      await handleJoinTribe({
+        tribeId: tribeIdToJoin,
+        userId: user.id,
+        joinTribeMutation: joinTribe,
+      });
+      navigator.navigate(PAGES.HOME as PageNameType);
     } catch (error) {
       console.warn('An error has occured');
     }

6c802cfa85a36a65b6e63545068a5847a2c8973a
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-01-05 00:31:57 +0100

    feat: Add checkbox for notifications

--- src/pages/Reminders/Reminders.component.tsx
+++ src/pages/Reminders/Reminders.component.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Text, TextStyle, StyleSheet, ViewStyle } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { onScroll } from 'react-native-redash';
+import CheckBox from 'react-native-check-box';
 import { Page, Loader } from '../../components';
 import {
   AnimatedHeader,
@@ -10,12 +11,9 @@ import {
 import theme from './../../theme';
 import { useCurrentUser } from 'pet-feeder/src/hooks';
 
-interface State {
-  scrollY: Animated.Value<number>;
-}
-
 export const Reminders: React.FC<{}> = () => {
   const [scrollY] = useState<Animated.Value<number>>(new Animated.Value(0));
+  const [notificationsAllowed, setNotificationsAllowed] = useState<boolean>(true);
   const { user } = useCurrentUser();
   if (!user) {
     return <Loader size={100} />;
@@ -27,7 +25,17 @@ export const Reminders: React.FC<{}> = () => {
         onScroll={onScroll({ y: scrollY })}
         contentContainerStyle={styles.container}
       >
-        <Text style={styles.text}>Content</Text>
+        <Text style={styles.text}>Mes Rappels</Text>
+        <CheckBox
+          style={styles.checkboxContainer}
+          onClick={() => {
+            setNotificationsAllowed(!notificationsAllowed);
+          }}
+          checkBoxColor={theme.colors.secondaryAction}
+          isChecked={notificationsAllowed}
+          leftTextStyle={styles.checkboxText}
+          leftText={'Recevoir une notifications lorsque mon chat a été nourri.e'}
+        />
       </Animated.ScrollView>
       <AnimatedHeader
         scrollY={scrollY}
@@ -48,6 +56,8 @@ export const Reminders: React.FC<{}> = () => {
 interface Style {
   container: ViewStyle;
   text: TextStyle;
+  checkboxContainer: ViewStyle;
+  checkboxText: TextStyle;
 }
 
 const styles = StyleSheet.create<Style>({
@@ -60,6 +70,15 @@ const styles = StyleSheet.create<Style>({
   text: {
     ...theme.fonts.regular,
     fontSize: 20,
-    paddingVertical: 10,
+    paddingVertical: 3 * theme.margins.unit,
+  },
+  checkboxContainer: {
+    flex: 1,
+    paddingVertical: 2 * theme.margins.unit,
+    borderBottomWidth: 1,
+  },
+  checkboxText: {
+    color: theme.colors.text,
+    marginRight: 4 * theme.margins.unit,
   },
 });

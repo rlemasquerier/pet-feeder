a91c4e899d20eabcedfa8f858a773e69d03291b5
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-02-22 23:19:15 +0100

    feat: Connect litter button to backend

--- src/pages/CustomActions/CustomActions.tsx
+++ src/pages/CustomActions/CustomActions.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { StyleSheet, View, ViewStyle, TextStyle } from 'react-native';
 import { Page, LargeButton, Header } from 'pet-feeder/src/components';
 import theme from 'pet-feeder/src/theme';
+import { useMutation } from 'react-apollo';
+import { createRecord } from 'pet-feeder/src/graphql/mutations';
+import { navigator } from 'pet-feeder/src/services/navigation';
 
 interface Props {}
 
 export const CustomActions: React.FC<Props> = () => {
+  const [addRecord, addRecordMutationResult] = useMutation(createRecord);
+  const addRecordLoading = addRecordMutationResult.loading;
+
   return (
     <Page>
       <Header title="Autres actions" />
@@ -13,7 +19,11 @@ export const CustomActions: React.FC<Props> = () => {
         <LargeButton
           label={'Litière'.toUpperCase()}
           color={theme.colors.secondary}
-          onPress={() => {}}
+          loading={addRecordLoading}
+          onPress={async () => {
+            await addRecord({ variables: { type: 'litter' } });
+            navigator.back();
+          }}
         />
       </View>
     </Page>

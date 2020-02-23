35e457c56d8d38587e0c9f45bd034a96cb5085fe
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-02-23 23:26:28 +0100

    fix: Use correct wording to be iso with graphql query and cache structure

--- src/pages/Home/components/DayScrollView/utils/index.ts
+++ src/pages/Home/components/DayScrollView/utils/index.ts
@@ -1,10 +1,11 @@
 import { Record } from 'pet-feeder/src/types';
+import { MORNING, EVENING } from 'pet-feeder/src/services/computeDayHalf';
 
 export const arrangeRecords = (morningRecords: Record[], eveningRecords: Record[]) => {
   return {
     food: {
-      morning: morningRecords.filter((record: Record) => record.type === 'food'),
-      evening: eveningRecords.filter((record: Record) => record.type === 'food'),
+      [MORNING]: morningRecords.filter((record: Record) => record.type === 'food'),
+      [EVENING]: eveningRecords.filter((record: Record) => record.type === 'food'),
     },
     others: [
       ...morningRecords.filter((record: Record) => record.type !== 'food'),

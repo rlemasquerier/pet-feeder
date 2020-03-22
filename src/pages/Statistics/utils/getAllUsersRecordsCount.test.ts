305ddc89964ad255661decf572312b19a9355168
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-03-22 16:29:07 +0100

    feat: Get count by users more robust, showing user from tribe with no records

--- src/pages/Statistics/utils/getAllUsersRecordsCount.test.ts
+++ src/pages/Statistics/utils/getAllUsersRecordsCount.test.ts
@@ -1,6 +1,24 @@
-import { getAllUsersRecordsCount } from './getAllUsersRecordsCount';
+import { getAllUsersRecordsCount, getUserRecordsCountById } from './getAllUsersRecordsCount';
 
 describe('[Service] getAllUsersRecordsCount', () => {
+  const mockedUsers = [
+    {
+      id: '5d9663c54cd54d34075751d4',
+      name: 'James',
+    },
+    {
+      id: '5d9663c54cd54d34075751d5',
+      name: 'Jean-Michel',
+    },
+    {
+      id: '5d9663c54cd54d34075751d6',
+      name: 'Zidane',
+    },
+    {
+      id: '12345',
+      name: 'InvisibleMan',
+    },
+  ];
   const mockedUserRecords = [
     {
       id: '1',
@@ -39,16 +57,31 @@ describe('[Service] getAllUsersRecordsCount', () => {
       timestamp: 1,
     },
   ];
-  it('should return the correct stats values', () => {
-    const actual = getAllUsersRecordsCount(mockedUserRecords);
-    const expected = [
-      {
-        user: 'James',
-        count: 2,
-      },
-      { user: 'Jean-Michel', count: 3 },
-      { user: 'Zidane', count: 1 },
-    ];
-    expect(actual).toEqual(expected);
+  describe('[Function] getUserRecordsCountById', () => {
+    it('should return the correct count if the user has records', () => {
+      const actual = getUserRecordsCountById(mockedUserRecords, '5d9663c54cd54d34075751d5');
+      const expected = 3;
+      expect(actual).toEqual(expected);
+    });
+    it('should return the correct count if the user has no records', () => {
+      const actual = getUserRecordsCountById(mockedUserRecords, '12345');
+      const expected = 0;
+      expect(actual).toEqual(expected);
+    });
+  });
+  describe('[Function] getAllUsersRecordsCount', () => {
+    it('should return the correct stats values', () => {
+      const actual = getAllUsersRecordsCount(mockedUserRecords, mockedUsers);
+      const expected = [
+        {
+          user: 'James',
+          count: 2,
+        },
+        { user: 'Jean-Michel', count: 3 },
+        { user: 'Zidane', count: 1 },
+        { user: 'InvisibleMan', count: 0 },
+      ];
+      expect(actual).toEqual(expected);
+    });
   });
 });

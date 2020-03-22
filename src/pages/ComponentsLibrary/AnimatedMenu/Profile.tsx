16b4acb84aec519cfb50f1a027cf69903425b596
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-03-23 00:12:38 +0100

    feat: Link profile part of menu to gesture transition once opening animation transition is finished

--- src/pages/ComponentsLibrary/AnimatedMenu/Profile.tsx
+++ src/pages/ComponentsLibrary/AnimatedMenu/Profile.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { alpha, perspective } from './Constants';
 import { Content, width } from './Content';
-import Animated from 'react-native-reanimated';
-import { bInterpolate } from 'react-native-redash';
+import Animated, {
+  Value,
+  useCode,
+  block,
+  cond,
+  and,
+  neq,
+  diff,
+  set,
+  eq,
+  interpolate,
+} from 'react-native-reanimated';
+import { bInterpolate, onGestureEvent } from 'react-native-redash';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
 const MIN = ((-4 * width) / Math.PI) * alpha;
 const MAX = 0;
@@ -12,15 +24,36 @@ interface ProfileProps {
   transition: Animated.Node<number>;
 }
 
-export const Profile = ({ transition }: ProfileProps) => {
+export const Profile = ({ open, transition: openingTransition }: ProfileProps) => {
+  const transition = new Value(0);
+  const state = new Value(State.UNDETERMINED);
+  const translationX = new Value(0);
+  const velocityX = new Value(0);
+  const gestureHandler = onGestureEvent({ state, translationX, velocityX });
+  const gestureTransition = interpolate(translationX, {
+    inputRange: [MIN, MAX],
+    outputRange: [0, 1],
+  });
+  useCode(
+    () =>
+      block([
+        cond(and(open, neq(diff(openingTransition), 0)), set(transition, openingTransition)),
+        cond(eq(state, State.ACTIVE), [set(transition, gestureTransition)]),
+      ]),
+    [open, openingTransition, transition]
+  );
+
   const translateX = bInterpolate(transition, MIN, MAX);
+
   return (
-    <Animated.View
-      style={{
-        transform: [perspective, { translateX }],
-      }}
-    >
-      <Content />
-    </Animated.View>
+    <PanGestureHandler {...gestureHandler}>
+      <Animated.View
+        style={{
+          transform: [perspective, { translateX }],
+        }}
+      >
+        <Content />
+      </Animated.View>
+    </PanGestureHandler>
   );
 };

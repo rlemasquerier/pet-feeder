dcea009d496e99a4984ca5ff065d34079a5b14e1
MaxLmqr <max.lemasquerier@protonmail.com>
2020-03-29 22:59:35 +0200

    destructuration dans une fonction, et quelques améliorations

--- src/redux/authentication/saga.ts
+++ src/redux/authentication/saga.ts
@@ -7,6 +7,27 @@ import { navigator } from '../../services/navigation';
 import { PAGES } from 'pet-feeder/src/services/navigation';
 import { showError } from 'pet-feeder/src/services/toaster.ts';
 
+// eslint-disable-next-line
+function handleLoginError(error: any) {
+  if (error.response == undefined) {
+    showError('Oups, il semble que tu sois déconnecté');
+  } else {
+    switch (error.response.status) {
+      case 403:
+        showError('Veuillez entrer vos identifiants.');
+        break;
+      case 404:
+        showError("Informations d'identifications non valides. Veuillez réessayer.");
+        break;
+      default:
+        showError('Une erreur est survenue.');
+        break;
+    }
+  }
+  // eslint-disable-next-line no-console
+  console.warn('[loginSaga/error]', error.message);
+}
+
 export function* loginSaga(action: LoginRequestAction): SagaIterator {
   try {
     const { email, password } = action.payload;
@@ -22,22 +43,7 @@ export function* loginSaga(action: LoginRequestAction): SagaIterator {
     }
   } catch (error) {
     yield put(apiCallError('authentication', error));
-    if (error.response == undefined) {
-      showError("Problème de connexion au serveur. Vérifiez votre connexion ou l'état du serveur.");
-    } else {
-      switch (error.response.status) {
-        case 403: {
-          showError('Veuillez entrer vos identifiants.');
-          break;
-        }
-        case 404: {
-          showError("Informations d'identifications non valides. Veuillez réessayer.");
-          break;
-        }
-      }
-    }
-    // eslint-disable-next-line no-console
-    console.warn('[loginSaga/error]', error.message);
+    handleLoginError(error);
   }
 }
 

f1939b4b52ee49eb989a899babf070d4fc6244a3
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-04-12 19:15:26 +0200

    refactor: Remove no longer needed injected js in webview

--- src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -5,24 +5,10 @@ import { Page, Header } from 'pet-feeder/src/components';
 interface Props {}
 
 export const PrivacyPolicy: React.FC<Props> = () => {
-  // TODO: It doesn't work without timeout. Why ?
-  const INJECTED_JAVASCRIPT = `
-    setTimeout(() => {
-      header = document.getElementById('header');
-      header.parentNode.removeChild(header);
-      footer = document.getElementById('footer');
-      footer.parentNode.removeChild(footer);
-    }, 100);
-    true;   
-  `;
-
   return (
     <Page>
       <Header title="Politique de confidentialité" />
-      <WebView
-        source={{ uri: 'https://rlemasquerier.github.io/pet-feeder/privacy-policy/' }}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
-      />
+      <WebView source={{ uri: 'https://rlemasquerier.github.io/pet-feeder/privacy-policy/' }} />
     </Page>
   );
 };

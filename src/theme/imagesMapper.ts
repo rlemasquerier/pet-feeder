8090545746408ae96db59b8315682e3d43c96203
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-04-27 21:56:18 +0200

    refactor: Remove unused asset

--- src/theme/imagesMapper.ts
+++ src/theme/imagesMapper.ts
@@ -1,6 +1,5 @@
 const images = {
   loginBackground: require('./images/login-background.png'),
-  cheersGif: require('./images/cheers.gif'),
   profilePicturePlaceholder: require('./images/profile-picture-placeholder.jpg'),
 };
 

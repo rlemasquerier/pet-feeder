fca74e5f433086fa7fcfdca3fb1e5610bdfb4c04
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-04-27 22:31:20 +0200

    feat: Add signup page design

--- src/theme/imagesMapper.ts
+++ src/theme/imagesMapper.ts
@@ -1,5 +1,7 @@
 const images = {
   loginBackground: require('./images/login-background.png'),
+  signupBackgroundTop: require('./images/signup-background-top.png'),
+  signupBackgroundBottom: require('./images/signup-background-bottom.png'),
   profilePicturePlaceholder: require('./images/profile-picture-placeholder.jpg'),
 };
 

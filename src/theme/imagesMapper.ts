4ce3b59bef859c49439fc8f813988792be4f912d
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-04-29 19:10:58 +0200

    feat: Add medals assets

--- src/theme/imagesMapper.ts
+++ src/theme/imagesMapper.ts
@@ -3,6 +3,9 @@ const images = {
   signupBackgroundTop: require('./images/signup-background-top.png'),
   signupBackgroundBottom: require('./images/signup-background-bottom.png'),
   profilePicturePlaceholder: require('./images/profile-picture-placeholder.jpg'),
+  bronzeMedal: require('./images/bronze-medal.png'),
+  silverMedal: require('./images/silver-medal.png'),
+  goldMedal: require('./images/gold-medal.png'),
 };
 
 export default images;

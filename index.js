709c158ca5f9b9a38d1a4cac7aeef233ed783df2
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-05-02 20:05:56 +0200

    fix: Android PanGestureHandler crash

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Fix android crash
+// https://github.com/software-mansion/react-native-gesture-handler/issues/320
+import 'react-native-gesture-handler';
+
 import { AppRegistry } from 'react-native';
 import { App } from './src/App';
 import { name as appName } from './app.json';

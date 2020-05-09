c9bed06d417f9f895e79248796dee2e0826681c1
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-05-09 18:41:45 +0200

    feat: Sync selected custom actions with backend

--- src/graphql/mutations/updateTribeCustomActions.ts
+++ src/graphql/mutations/updateTribeCustomActions.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const updateTribeCustomActions = gql`
-  mutation patchTribe($id: String!, $customActions: [String]!) {
-    patchTribe(input: { id: $id, customActions: $customActions }) {
+  mutation patchTribe($id: String!, $customActions: [String!]) {
+    patchTribe(input: { tribeId: $id, customActions: $customActions }) {
       id
       customActions {
         id
